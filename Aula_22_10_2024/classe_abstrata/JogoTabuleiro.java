74db7c1969f1f4e83e9e5b86dd8c148f24d8b63a
Ranyelson Neres <ranyelson.carvalho@p.ucb.br>
2024-10-22 21:23:17 -0300

    Create JogoTabuleiro.java

---
+++ Aula_22_10_2024/classe_abstrata/JogoTabuleiro.java
@@ -0,0 +1,20 @@
+package abstrato;
+
+public class JogoTabuleiro extends Jogo {
+
+	String tipoTabuleiro;
+
+	public JogoTabuleiro(String nome, int nJogadores, String tipoTabuleiro) {
+		super(nome, nJogadores);
+		this.tipoTabuleiro = tipoTabuleiro;
+	}
+	
+	public void iniciar() {
+		
+	}
+	
+	public void regras() {
+		
+	}
+	
+}
